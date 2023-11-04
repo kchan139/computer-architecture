51bb7691894b4459bb2c18192b27ee1bb9145afa
Khoa Tran <khoatrandd.139@gmail.com>
2023-11-04 21:06:49 +0700

    added 02

--- LAB1/Exercise_02.asm
+++ LAB1/Exercise_02.asm
@@ -1,70 +1,24 @@
-    .data
-array:  .space 80   # Array of 20 integers, each integer takes 4 bytes (20 * 4 = 80)
-prompt: .asciiz "Enter 20 integers: "
-newline: .asciiz "\n"
-
+.data
+array: .word 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20
+size: .word 20
 
 .text
-.globl main
-
 main:
-    li $t0, 20           # Initialize $t0 to 20 (number of elements)
-    la $t1, array        # Load base address of array into $t1
+    la $t0, array
+    lw $t1, size
+    sll $t1, $t1, 2
+    add $t1, $t1, $t0
 
-    # Prompt user to enter 20 integers
-    li $v0, 4
-    la $a0, prompt
-    syscall
+reverse:
+    subu $t1, $t1, 4
+    lw $t2, 0($t0)
+    lw $t3, 0($t1)
+    sw $t2, 0($t1)
+    sw $t3, 0($t0)
+    addiu $t0, $t0, 4
+    slt $t4, $t0, $t1
+    bnez $t4, reverse
 
-    # Loop to input 20 integers into the array
-input_loop:
-    beq $t0, $zero, reverse_array  # If $t0 is 0, go to reversing the array
-
-    li $v0, 5            # Read integer
-    syscall
-    sw $v0, 0($t1)       # Store integer in the array
-    addi $t1, $t1, 4     # Move to the next element in the array
-    sub $t0, $t0, 1      # Decrement the counter
-    j input_loop          # Repeat input loop
-
-reverse_array:
-    la $t1, array         # Re-initialize $t1 to the base address of the array
-    li $t2, 0            # Initialize $t2 to 0 (start index)
-    li $t3, 19           # Initialize $t3 to 19 (end index)
-
-reverse_loop:
-    bge $t2, $t3, print_reversed_array  # If start index is greater than or equal to end index, print the reversed array
-
-    lw $t4, 0($t1)       # Load value at start index
-    lw $t5, 0($t1)       # Load value at end index
-    sw $t5, 0($t1)       # Store value at start index in place of value at end index
-    sw $t4, 0($t1)       # Store value at end index in place of value at start index
-
-    addi $t1, $t1, 4     # Move to the next element from the beginning
-    addi $t2, $t2, 1     # Increment start index
-    sub $t3, $t3, 1      # Decrement end index
-    j reverse_loop        # Repeat reverse loop
-
-print_reversed_array:
-    la $t1, array         # Re-initialize $t1 to the base address of the array
-
-    # Print the reversed array
-    li $t0, 20            # Re-initialize $t0 to 20 (number of elements)
-
-print_loop:
-    beq $t0, $zero, end_program  # If $t0 is 0, end the program
-
-    lw $a0, 0($t1)        # Load value to print
-    li $v0, 1
-    syscall
-    li $v0, 4
-    la $a0, newline       # Print newline for formatting
-    syscall
-
-    addi $t1, $t1, 4      # Move to the next element in the array
-    sub $t0, $t0, 1       # Decrement the counter
-    j print_loop           # Repeat print loop
-
-end_program:
-    li $v0, 10             # Exit syscall
+exit:
+    li $v0, 10
     syscall
